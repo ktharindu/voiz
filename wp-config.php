ee13277781a827e29116103f921f5f9c6f68a6f7
uktfernando <tfernando@netstarter.com>
2016-10-29 23:43:59 +0530

    Initial Commit

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'voiz');
 define('DB_USER', 'voiz');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'voiz');
+define('DB_PASSWORD', 'voiz@123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
