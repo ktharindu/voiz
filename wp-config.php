e287578d932a0c15e0ff7446c4b117d33c443578
uktfernando <tfernando@netstarter.com>
2016-10-29 23:47:08 +0530

    Initial Commit

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'voiz');
+define('DB_NAME', 'voiz'); //u673485527_voiz
 
 /** MySQL database username */
-define('DB_USER', 'voiz');
+define('DB_USER', 'voiz'); //u673485527_voiz
 
 /** MySQL database password */
 define('DB_PASSWORD', 'voiz@123');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost'); //mysql.hostinger.co.uk
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
